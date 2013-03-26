80ce9ab373f00d6a9c89c055fadbe4bb6ad9d52a
235471 <ioliveirap90@gmail.com>
2013-03-25 22:26:47 -0300

    First commit

---
+++ JSF1/src/br/com/view/JSF.java
@@ -0,0 +1,12 @@
+package br.com.view;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="jsf")
+public class JSF {
+
+	public String getMessage()
+	{
+	return "Israel Oliveira Pinto";
+	}
+}
